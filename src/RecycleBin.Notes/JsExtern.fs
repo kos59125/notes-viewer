139dea32f8da1a9abc4781d880e2d3ccd532d270
Abe Kosei <mail@recyclebin.jp>
2020-04-19 02:25:55 +0900

    parameterize document title and favicon

--- src/RecycleBin.Notes/JsExtern.fs
+++ src/RecycleBin.Notes/JsExtern.fs
@@ -5,6 +5,12 @@ open Microsoft.JSInterop
 
 type IJSRuntime with
 
+   member js.setTitle(title:string) =
+      js.InvokeVoidAsync("RecycleBin.Notes.setTitle", title).AsTask() |> Async.AwaitTask
+
+   member js.setIcon(icon:Icon) =
+      js.InvokeVoidAsync("RecycleBin.Notes.setIcon", icon.IconUrl, icon.MediaType).AsTask() |> Async.AwaitTask
+
    member js.HighlightHtml() =
       js.InvokeVoidAsync("RecycleBin.Notes.highlight").AsTask() |> Async.AwaitTask
 
--- src/RecycleBin.Notes/wwwroot/js/script.js
+++ src/RecycleBin.Notes/wwwroot/js/script.js
@@ -1,5 +1,23 @@
 window.RecycleBin = {
   Notes: {
+    setTitle: (title) => {
+      document.title = title;
+    },
+
+    setIcon: (url, type) => {
+      const head = document.getElementsByTagName("head")[0];
+      const icon = document.createElement("link");
+      icon.rel = "icon";
+      icon.href = url;
+      icon.type = type;
+
+      const existingIcon = head.querySelector("head link[rel='icon']");
+      if (existingIcon) {
+        head.removeChild(existingIcon);
+      }
+      head.appendChild(icon);
+    },
+
     highlight: () => {
       document.querySelectorAll("pre code").forEach(hljs.highlightBlock);
       document.querySelectorAll(".math").forEach(MathJax.typeset);
