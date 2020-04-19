3c9902b5d28272ccadf35da7933e04deded14f9b
Abe Kosei <mail@recyclebin.jp>
2020-04-19 21:17:14 +0900

    add copy button

--- src/RecycleBin.Notes/wwwroot/js/script.js
+++ src/RecycleBin.Notes/wwwroot/js/script.js
@@ -19,8 +19,35 @@ window.RecycleBin = {
     },
 
     highlight: () => {
+      // highlight
+      document.querySelectorAll("pre").forEach((e, index) => {
+        // brefore: <pre></pre>
+        // after: 
+        // <div class="codeblock">
+        //   <pre id="codeblock-{id}"></pre>
+        //   <button class="button is-small is-text trigger-clipboard" data-clipboard-target="#codeblock-{index}">
+        //     Copy
+        //   </button>
+        // </div>
+        const wrapper = document.createElement("div");
+        wrapper.classList.add("codeblock");
+
+        e.id = e.id || `codeblock-${index}`;
+        e.parentNode.insertBefore(wrapper, e);
+        wrapper.appendChild(e);
+
+        const button = document.createElement("button");
+        button.classList.add("button", "is-small", "is-text", "trigger-clipboard");
+        button.dataset.clipboardTarget = `#${e.id}`;
+        wrapper.appendChild(button);
+
+        const copyText = document.createTextNode("Copy");
+        button.appendChild(copyText);
+      });
       document.querySelectorAll("pre code").forEach(hljs.highlightBlock);
       document.querySelectorAll("code.hljs").forEach(hljs.lineNumbersBlock);
+
+      // math
       document.querySelectorAll(".math").forEach(MathJax.typeset);
     },
 
